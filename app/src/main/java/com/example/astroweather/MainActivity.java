8f21ed68e1895d4dcdfb525f409dad8740fcb0b6
kapis18 <kapis1881@gmail.com>
2020-04-26 21:53:59 +0200

    Displaying current time according to users timezone

--- app/src/main/java/com/example/astroweather/MainActivity.java
+++ app/src/main/java/com/example/astroweather/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.astroweather;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextClock;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.TimeZone;
+
+public class MainActivity extends Activity {
+    private TextClock currentTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        currentTime = findViewById(R.id.currentTime);
+        currentTime.setTimeZone(TimeZone.getDefault().getID());
     }
 }
