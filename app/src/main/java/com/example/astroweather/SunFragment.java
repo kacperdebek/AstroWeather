3aa6ed61f7d86c51d18635b92b2662f1e19d5418
kapis18 <kapis1881@gmail.com>
2020-04-28 17:49:13 +0200

    Displaying all information for sun

--- app/src/main/java/com/example/astroweather/SunFragment.java
+++ app/src/main/java/com/example/astroweather/SunFragment.java
@@ -9,12 +9,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class SunFragment extends Fragment {
-    private TextView sunrise;
-    private TextView sunset;
-    private TextView azimuthRise;
-    private TextView azimuthSet;
-    private TextView twilightMorning;
-    private TextView twilightEvening;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +19,31 @@ public class SunFragment extends Fragment {
                              Bundle savedInstanceState) {
         MainActivity activity = (MainActivity) getActivity();
         View view = inflater.inflate(R.layout.fragment_sun, container, false);
-        sunrise = view.findViewById(R.id.sunrise);
 
-        sunrise.setText(activity.sunInfo.getSunrise().toString());
+        TextView sunrise = view.findViewById(R.id.sunrise);
+        String sunriseText = "Sunrise: " + activity.sunInfo.getSunrise().toString();
+        sunrise.setText(sunriseText);
+
+        TextView sunset = view.findViewById(R.id.sunset);
+        String sunsetText = "Sunset: " + activity.sunInfo.getSunset().toString();
+        sunset.setText(sunsetText);
+
+        TextView azimuthRise = view.findViewById(R.id.azimuthrise);
+        String azimuthRiseText = "Azimuth rise: " + activity.sunInfo.getAzimuthRise();
+        azimuthRise.setText(azimuthRiseText);
+
+        TextView azimuthSet = view.findViewById(R.id.azimuthset);
+        String azimuthSetText = "Azimuth set: " + activity.sunInfo.getAzimuthSet();
+        azimuthSet.setText(azimuthSetText);
+
+        TextView twilightMorning = view.findViewById(R.id.twilightmorning);
+        String twilightMorningText = "Twilight morning: " + activity.sunInfo.getTwilightMorning().toString();
+        twilightMorning.setText(twilightMorningText);
+
+        TextView twilightEvening = view.findViewById(R.id.twilightevening);
+        String twilightEveningText = "Twilight evening: " + activity.sunInfo.getTwilightEvening().toString();
+        twilightEvening.setText(twilightEveningText);
+
         return view;
     }
 
