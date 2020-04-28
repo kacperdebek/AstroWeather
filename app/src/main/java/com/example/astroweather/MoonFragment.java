a80ceec8009ae2171944771a201e28e7e94159d3
kapis18 <kapis1881@gmail.com>
2020-04-28 18:08:14 +0200

    Displaying all information for moon

--- app/src/main/java/com/example/astroweather/MoonFragment.java
+++ app/src/main/java/com/example/astroweather/MoonFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MoonFragment extends Fragment {
 
@@ -17,7 +18,34 @@ public class MoonFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_moon, container, false);
+        MainActivity activity = (MainActivity) getActivity();
+        View view = inflater.inflate(R.layout.fragment_moon, container, false);
+
+        TextView moonrise = view.findViewById(R.id.moonrise);
+        String moonriseText = "Moonrise: " + activity.moonInfo.getMoonrise().toString();
+        moonrise.setText(moonriseText);
+
+        TextView moonset = view.findViewById(R.id.moonset);
+        String moonsetText = "Moonset: " + activity.moonInfo.getMoonset().toString();
+        moonset.setText(moonsetText);
+
+        TextView nextFullMoon = view.findViewById(R.id.nextfullmoon);
+        String nextFullMoonText = "Next full moon: " + activity.moonInfo.getNextFullMoon().toString();
+        nextFullMoon.setText(nextFullMoonText);
+
+        TextView nextNewMoon = view.findViewById(R.id.nextnewmoon);
+        String nextNewMoonText = "Next new moon: " + activity.moonInfo.getNextNewMoon().toString();
+        nextNewMoon.setText(nextNewMoonText);
+
+        TextView illumination = view.findViewById(R.id.illumination);
+        String illuminationText = "Illumination %: " + activity.moonInfo.getIllumination();
+        illumination.setText(illuminationText);
+
+        TextView moonAge = view.findViewById(R.id.moonage);
+        String moonAgeText = "Moon age: " + activity.moonInfo.getAge();
+        moonAge.setText(moonAgeText);
+
+        return view;
     }
 
 }
